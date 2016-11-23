004d1ad74f6de43552baef67446864477e77235e
ashutosh <ashutoshraina1989@gmail.com>
2016-11-23 03:22:00 -0800

    initial stab at some metric of the graphs being generated.

--- src/main/kotlin/org/slug/Main.kt
+++ src/main/kotlin/org/slug/Main.kt
@@ -1,15 +1,15 @@
 package org.slug
 
+import org.graphstream.algorithm.Toolkit.*
 import org.graphstream.algorithm.measure.ChartMeasure
 import org.graphstream.algorithm.measure.ChartSeries2DMeasure
+import org.graphstream.graph.Graph
 import org.graphstream.graph.implementations.SingleGraph
-import org.graphstream.util.cumulative.GraphSpells
 import org.slug.core.CrossTalkGenerator
 import org.slug.core.MicroserviceGenerator
 import org.slug.output.DisplayHelper
 import org.slug.output.display
 import org.slug.output.generateDotFile
-import java.util.*
 
 
 class Main {
@@ -24,11 +24,11 @@ class Main {
                 else -> DisplayHelper().loadDefaultCSS()
             }
 
-            generator(css, simpleArchitecture())
-            generator(css, simple3Tier())
-            generator(css, multipleLinks())
-            generator(css, e2e())
-            generator(css, e2eMultipleApps())
+            val simpleGraphs: Sequence<Graph> = emptySequence<SingleGraph>().plusElement(generator(css, simpleArchitecture()))
+                    .plusElement(generator(css, simple3Tier()))
+                    .plusElement(generator(css, multipleLinks()))
+                    .plusElement(generator(css, e2e()))
+                    .plusElement(generator(css, e2eMultipleApps()))
 
             val architecture = multiService()
             val serviceGraphs = architecture.generators().map { microservice -> generator(css, microservice) }
@@ -40,6 +40,35 @@ class Main {
                         if (config.getBooleanProperty("display.swing")) display(graph)
                         if (config.getBooleanProperty("display.dot")) generateDotFile(graph)
                     }
+
+            val graphs = simpleGraphs.plus(serviceGraphs)
+            val m1 = ChartSeries2DMeasure("Density measure")
+            (0..graphs.count() - 1).forEach { r ->
+                m1.addValue((r + 1).toDouble(), density(graphs.elementAt(r)))
+            }
+
+            val params = ChartMeasure.PlotParameters()
+            params.title = "Density Scatter Plot"
+            params.type = ChartMeasure.PlotType.LINE
+            m1.plot(params)
+
+            val m2 = ChartSeries2DMeasure("Average Degree measure")
+            (0..graphs.count() - 1).forEach { r ->
+                m2.addValue((r + 1).toDouble(), averageDegree(graphs.elementAt(r)))
+            }
+
+            params.title = "Average Degree Scatter Plot"
+            params.type = ChartMeasure.PlotType.LINE
+            m2.plot(params)
+
+            val m3 = ChartSeries2DMeasure("Average Degree Deviation")
+            (0..graphs.count() - 1).forEach { r ->
+                m3.addValue((r + 1).toDouble(), degreeAverageDeviation(graphs.elementAt(r)))
+            }
+
+            params.title = "Average Degree Deviation"
+            params.type = ChartMeasure.PlotType.BAR
+            m3.plot(params)
         }
 
         fun generator(css: String, generator: MicroserviceGenerator): SingleGraph {
