5396f86a20832771751f862d19603657ab09bdd0
ashutosh raina <ashutoshraina1989@gmail.com>
2017-02-07 14:18:41 -0800

    added static imports.

--- src/main/kotlin/org/slug/core/LayerConnection.kt
+++ src/main/kotlin/org/slug/core/LayerConnection.kt
@@ -1,46 +1,50 @@
 package org.slug.core
 
+import org.slug.core.InfrastructureType.*
+import org.slug.core.Protocol.HTTP
+import org.slug.core.Protocol.TCP
+
 sealed class LayerConnection {
   abstract val from: InfrastructureType
   abstract val to: InfrastructureType
   abstract val outDegree: Int
   abstract val protocol: Protocol
 
-  class CDN2Firewall(override val from: InfrastructureType.CDN,
-                     override val to: InfrastructureType.Firewall,
+  class CDN2Firewall(override val from: CDN,
+                     override val to: Firewall,
                      override val outDegree: Int,
-                     override val protocol: Protocol = Protocol.TCP) : LayerConnection()
+                     override val protocol: Protocol = TCP) : LayerConnection()
 
-  class Firewall2LoadBalancer(override val from: InfrastructureType.Firewall,
-                              override val to: InfrastructureType.LoadBalancer,
+  class Firewall2LoadBalancer(override val from: Firewall,
+                              override val to: LoadBalancer,
                               override val outDegree: Int,
-                              override val protocol: Protocol = Protocol.TCP) : LayerConnection()
+                              override val protocol: Protocol = TCP) : LayerConnection()
 
-  class LoadBalancer2Proxy(override val from: InfrastructureType.LoadBalancer,
-                           override val to: InfrastructureType.Proxy,
+  class LoadBalancer2Proxy(override val from: LoadBalancer,
+                           override val to: Proxy,
                            override val outDegree: Int,
-                           override val protocol: Protocol = Protocol.HTTP) : LayerConnection()
+                           override val protocol: Protocol = HTTP) : LayerConnection()
 
-  class Proxy2WebApplication(override val from: InfrastructureType.Proxy,
-                             override val to: InfrastructureType.WebApplication,
+  class Proxy2WebApplication(override val from: Proxy,
+                             override val to: WebApplication,
                              override val outDegree: Int,
-                             override val protocol: Protocol = Protocol.HTTP) : LayerConnection()
+                             override val protocol: Protocol = HTTP) : LayerConnection()
 
-  class WebApplication2Cache(override val from: InfrastructureType.WebApplication,
-                             override val to: InfrastructureType.Cache,
+  class WebApplication2Cache(override val from: WebApplication,
+                             override val to: Cache,
                              override val outDegree: Int,
-                             override val protocol: Protocol = Protocol.TCP) : LayerConnection()
+                             override val protocol: Protocol = TCP) : LayerConnection()
 
-  class Cache2Database(override val from: InfrastructureType.Cache,
-                       override val to: InfrastructureType.Database,
+  class Cache2Database(override val from: Cache,
+                       override val to: Database,
                        override val outDegree: Int,
-                       override val protocol: Protocol = Protocol.TCP) : LayerConnection()
+                       override val protocol: Protocol = TCP) : LayerConnection()
 
-  class ServiceDiscoveryIndirection(override val from: InfrastructureType.DiscoverableInfrastructureType,
-                                    val via: InfrastructureType.ServiceDiscovery,
-                                    override val to: InfrastructureType.DiscoverableInfrastructureType,
+  class ServiceDiscoveryIndirection(override val from: DiscoverableInfrastructureType,
+                                    val via: ServiceDiscovery,
+                                    override val to: DiscoverableInfrastructureType,
                                     override val outDegree: Int = 1,
-                                    override val protocol: Protocol = Protocol.TCP) : LayerConnection() {
+                                    override val protocol: Protocol = TCP) : LayerConnection() {
     override fun toString(): String {
       return "ServiceDiscoveryIndirection(from=$from, via=$via, to=$to, outDegree=$outDegree)"
     }
